f6d96351b02957eb84a438b329844ef47671a4c1
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-10-24 17:41:00 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+        const menuHamburger = document.querySelector(".menu-hamburger")
+        const navLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
